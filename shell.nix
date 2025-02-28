ff93453dc59b8353670adfb0d6cb82868840b623
raf <rraf@tuta.io>
2025-02-28 17:42:38 +0000

    update: initialised project

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+pkgs.mkShell {
+  packages = [
+    pkgs.python312
+    pkgs.python312Packages.pip
+    (pkgs.writeShellScriptBin "notebook" ''
+      #!${pkgs.stdenv.shell}
+      jupyter-notebook "$@"
+    '')
+  ];
+  shellHook = ''
+    python -m venv .venv
+    source .venv/bin/activate
+
+    pip install numpy
+    pip install pandas
+    pip install matplotlib
+    pip install seaborn
+  '';
+}
